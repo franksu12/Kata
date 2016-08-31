d567ba9e3134d27cd1ddce799c6ba083dfa8a016
franksu12 <franksu12@gmail.com>
2016-08-31 07:57:31 -0400

    First Commit

---
+++ Kata/src/com/website/KataTest.java
@@ -0,0 +1,14 @@
+package com.website;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KataTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
